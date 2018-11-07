2a8b8ca383b2949c6c45b669e0f0ee5beb63fe6f
Shota Sugawara <shrikethrush@gmail.com>
2018-11-07 15:53:22 -0800

    Complete The functional-shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let newQueue = {};
+  newQueue.length = 0;
+  newQueue.frontKey = 0;
+  newQueue.lastKey = 0;
+  var extend = function(obj) {
+    return Object.assign(...arguments);
+  };
+
+  extend(newQueue, queueMethods); 
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+  return this.length;
+};
 
+queueMethods.enqueue = function(value) {
+  this[this.lastKey] = value;
+  this.length++;
+  this.lastKey++;
+};
+
+queueMethods.dequeue = function() {
+  let poppedValue = '';
+  if (this.length) {
+    poppedValue = this[this.frontKey];
+    delete this[this.frontKey];
+  
+    this.length--;
+    this.frontKey++;
+  } 
+
+  return poppedValue;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-};
+  let newStack = {};
+  newStack.length = Object.keys(newStack).length;
+
+  var extend = function(obj) {
+    return Object.assign(...arguments);
+  };
+
+  extend(newStack, stackMethods); 
+
+  return newStack;
+};	
+
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return this.length;
+};
 
+stackMethods.push = function(value) {
+  this[this.length] = value;
+  this.length++;
+};
+
+stackMethods.pop = function() {
+  let poppedValue = this[this.length - 1];
+  delete this[this.length - 1];
+  if (this.length) { this.length--; } 
+  return poppedValue;
+};
