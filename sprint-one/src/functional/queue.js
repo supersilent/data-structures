bc2a44e35d08d8179da44a20850ff7000beb0962
Shota Sugawara <shrikethrush@gmail.com>
2018-11-07 12:55:02 -0800

    Complete The functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  let count = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    let storageLength = Object.keys(storage).length;
+    let lastStorageKey = 0;
+    if (!storageLength) {
+      lastStorageKey = 0;
+    }else{
+      lastStorageKey = Number(Object.keys(storage)[storageLength - 1]) + 1;
+    }
+    storage[lastStorageKey] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    if (!count   ){ return '';}
+    let dequeuedValue = Object.keys(storage).length ? storage[Object.keys(storage)[0]] : '';
+    delete storage[Object.keys(storage)[0]];
+    count--;
+    return dequeuedValue;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,17 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[Object.keys(storage).length] = value;
   };
 
   someInstance.pop = function() {
+    let poppedValue = Object.keys(storage).length ? storage[Object.keys(storage).length - 1] : '';
+    delete storage[Object.keys(storage).length - 1];
+    return poppedValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
