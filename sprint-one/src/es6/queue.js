fc558cad2ca2af507aec8f7da88a4140361f5590
Shota Sugawara <shrikethrush@gmail.com>
2018-11-07 17:55:33 -0800

    Complete The es6 pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,34 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
+    this.frontKey = 0;
+    this.lastKey = 0;
+    this.storage = {};
   }
+
+  size () {
+    return this.length;
+  }
+
+  enqueue (value) {
+    this.storage[this.lastKey] = value;
+    this.length++;
+    this.lastKey++;
+  }
+
+  dequeue () {
+    let poppedValue = '';
+    if (this.length) {
+      poppedValue = this.storage[this.frontKey];
+      delete this.storage[this.frontKey];
+      this.length--;
+      this.frontKey++;
+    } 
+
+    return poppedValue;
+
+  }
+
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,28 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
+    this.storage = {};
+    // this.lastValue = '';
+  }
+
+  size() {
+    return this.length;
+  }
+  
+  push(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  }
+
+  pop() {
+    let poppedValue = '';
+    if (this.length) {
+      poppedValue = this.storage[this.length - 1];
+      delete this.storage[this.length - 1];
+      this.length--;
+    }
+    return poppedValue;
   }
 
 }
